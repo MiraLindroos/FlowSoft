2df9e4e11464abfaf56939500a6c606974e16846
MiraLindroos <mira.isabelle27@gmail.com>
2025-06-13 18:40:49 +0300

    styling the sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,21 +1,25 @@
 import "./Sidebar.css"
 import { Link } from "react-router-dom";
+import { FiCalendar, FiHome, FiFolder } from "react-icons/fi";
 
 const Sidebar = () => {
 
   const routes = [
-    { name: "Etusivu", path: "/Etusivu" },
-    { name: "Ajanhallinta", path: "/Ajanhallinta" },
-    { name: "Projektit", path: "/Projektit" },
+    { name: "Etusivu", path: "/Etusivu", IconName: <FiHome /> },
+    { name: "Ajanhallinta", path: "/Ajanhallinta", IconName: <FiCalendar /> },
+    { name: "Projektit", path: "/Projektit", IconName: <FiFolder /> },
   ]
 
   return (
     <div className="sidebar">
-      {routes.map((route, index) => (
-        <Link key={index} to={route.path}>
-          {route.name}
-        </Link>
-      ))}
+      <div className="sidebar-items">
+        {routes.map((route, index) => (
+          <Link key={index} to={route.path} className="item">
+            <span style={{marginRight: '10px', verticalAlign: 'middle'}}>{route.IconName}</span>{route.name}
+          </Link>
+        ))}
+      </div>
+      
     </div>
   )
 }
