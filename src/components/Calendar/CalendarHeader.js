0d440729839e487e871aa1817823fc2579b7006f
MiraLindroos <mira.isabelle27@gmail.com>
2025-06-23 16:12:35 +0300

    added weekdays to the calendar

--- src/components/Calendar/CalendarHeader.js
+++ src/components/Calendar/CalendarHeader.js
@@ -12,13 +12,22 @@ const CalendarHeader = (props) => {
     nextMonth()
   }
 
+  const weekdays = ['ma', 'ti', 'ke', 'to', 'pe', 'la', 'su'];
+
   return (
-    <div className="calendar-header">
-      <button className="header-button"onClick={clickLeft}>{'<<'}</button>
-      {getMonth()}
-      <button className="header-button" onClick={clickRight}>{'>>'}</button>
-    </div>
-  )
+    <>
+      <div className="calendar-header">
+        <button className="header-button" onClick={clickLeft}>{'<<'}</button>
+        {getMonth()}
+        <button className="header-button" onClick={clickRight}>{'>>'}</button>
+      </div>
+      <div className="calendar-weekdays">
+        {weekdays.map(day => (
+          <span className="calendar-weekday" key={day}>{day}</span>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default CalendarHeader;
