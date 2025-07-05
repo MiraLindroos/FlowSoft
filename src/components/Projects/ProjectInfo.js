387e56fd05fd2292e65a7ca959426512e6937931
MiraLindroos <mira.isabelle27@gmail.com>
2025-07-05 14:58:12 +0300

    more responsive styling for project detail

--- src/components/Projects/ProjectInfo.js
+++ src/components/Projects/ProjectInfo.js
@@ -1,7 +1,9 @@
+import "./Projects.css"
+
 const ProjectInfo = ({project}) => {
   return (
-    <div style={{textAlign: 'left', margin: ' 0 5px'}}>
-      <p style={{margin: '0'}}>Projekti: {project.name}</p>
+    <div className="project-info">
+      <p className="info-p">Projekti: {project.name}</p>
       <p>Kesto: {project.endDate.toDate().toDateString()}</p>
       <p>Lisätty: {project.created.toDate().toDateString()}</p>
       <p>Hinta: {project.price}</p>
