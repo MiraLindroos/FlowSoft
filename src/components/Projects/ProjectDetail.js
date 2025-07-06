ff9f73829902d7413e7ec42b6fbe0cfcb143558e
MiraLindroos <mira.isabelle27@gmail.com>
2025-07-06 10:46:21 +0300

    deleted projectInfo component and replaced it with cardSection component

--- src/components/Projects/ProjectDetail.js
+++ src/components/Projects/ProjectDetail.js
@@ -1,5 +1,5 @@
 import Card from "../Card/Card"
-import ProjectInfo from "./ProjectInfo"
+import CardSection from "../Card/CardSection"
 import "./Projects.css"
 
 const ProjectDetail = ({project}) => {
@@ -8,21 +8,39 @@ const ProjectDetail = ({project}) => {
       <div className="project-detail info">
         <Card
           title='projektin tiedot'
-        > <ProjectInfo project={project}/>
+        > <CardSection
+          fields={[
+            {label: "Projekti", value: project.name},
+            {label: "Kesto", value: project.endDate.toDate().toDateString()},
+            {label: "Lisätty", value: project.created.toDate().toDateString()},
+            {label: "Hinta", value: project.price}
+          ]}
+        />
         </Card>
       </div>
       <div className="project-detail hours">
         <Card
           title='projektin tunnit'
           description='tunnit'
-        > <ProjectInfo project={project}/>
+        > <CardSection
+            fields={[
+              {label: "Tunnit tässä kuussa", value: "20"},
+              {label: "tunnit yhteensä", value: "90"},
+            ]}
+          />
         </Card>
       </div>
       <div className="project-detail else">
         <Card
           title='projektin muuta'
           description='jotain'
-        > <ProjectInfo project={project}/>
+        > <CardSection
+            fields={[
+              {label: "Muuta tietoo", value: "testitesti"},
+              {label: "testi", value: "liibalaabaluuba"},
+              {label: "testi 3", value: "lorem ipsum...lorem ipsum...lorem ipsum..."},
+            ]}
+          />
         </Card>
       </div>
     </div>
