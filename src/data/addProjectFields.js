2e088da6bebb1390d32e921833e390ac191d738d
MiraLindroos <mira.isabelle27@gmail.com>
2025-07-12 18:28:53 +0300

    add new project button opening a form onclick

---
+++ src/data/addProjectFields.js
@@ -0,0 +1,10 @@
+const addProjectFields = [
+  {name:"name", label: "Projektin nimi", type: "text"},
+  {name:"startTime", label: "Alkamispäivä", type: "time"},
+  {name:"endTime", label: "Lopetuspäivä", type: "time"},
+  {name:"hourRate", label: "Tuntihinta", type: "number"},
+  {name:"price", label: "Kiinteä hinta", type: "number"},
+  {name:"memo", label: "Muistiinpanot", type: "textarea"},
+]
+
+export default addProjectFields
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,20 +1,62 @@
 import ProjectsList from "../../components/Projects/ProjectsList"
 import useProjectsList from "../../hooks/useProjectsList"
 import "./Projects.css"
+import Modal from "../../components/Modal/Modal"
+import { useForm, FormProvider } from "react-hook-form"
+import useModal from "../../hooks/useModal"
+import Form from "../../components/Forms/Form"
+import addProjectFields from "../../data/addProjectFields"
 
 const Projects = () => {
   const {
     projects
   } = useProjectsList()
 
+  const {
+    showModal,
+    modalContent,
+    openModal,
+    closeModal
+  } = useModal()
+
+  const methods = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
+    const addProjectClick = () => {
+      openModal({
+        message: "Lisää uusi projekti",
+        children:
+        <FormProvider {...methods}>
+          <Form fields={addProjectFields}/>
+        </FormProvider>,
+        onConfirm: methods.handleSubmit(onSubmit),
+        onCancel: closeModal,
+        cancelButton: "Peruuta",
+        confirmButton: "Tallenna",
+        width: "85%"
+      })
+    }
+
   return (
     <div className="projects-view">
       <div className="project-buttons">
-        <button className="add-project">LISÄÄ UUSI</button>
+        <button className="add-project" onClick={addProjectClick}>LISÄÄ UUSI</button>
       </div>
       <div className="projects-list">
         <ProjectsList projects={projects}/>
       </div>
+      {showModal &&
+        <Modal
+          message={modalContent.message}
+          children={modalContent.children}
+          onConfirm={modalContent.onConfirm}
+          onCancel={modalContent.onCancel}
+          cancelButton={modalContent.cancelButton}
+          confirmButton={modalContent.confirmButton}
+          width={modalContent.width}
+        />
+      }
     </div>
   )
 }
