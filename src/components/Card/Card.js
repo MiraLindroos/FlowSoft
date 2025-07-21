db27b9fef0f76dece95db3de2850dd15d69c58a2
MiraLindroos <mira.isabelle27@gmail.com>
2025-07-21 18:15:58 +0300

    added comments to card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,18 @@ import cardColors from "../../theme/cardColors"
 
 const Card = ({icon, title, description, children, variant = "default"}) => {
 
+  // Get card background and style from theme based on variant
   const styles = cardColors[variant] || cardColors.default
   return (
     <div className='card' style={styles}>
       <div className='card-info'>
+        {/* Show given icon and title */}
         <h3>{icon} {title}</h3>
+        {/* Display optional description */}
         <small>{description}</small>
       </div>
       <div className='card-content'>
+        {/* Card main content are */}
         {children}
       </div>
     </div>
--- src/components/Card/CardSection.js
+++ src/components/Card/CardSection.js
@@ -3,7 +3,9 @@ import "./Card.css"
 const CardSection = ({fields}) => {
   return (
     <div className="card-section">
+      {/* Map through fields and display them as <p> element */}
       {fields.map((field, index) =>
+      // Display field label and value
       <p key={index} className="card-section-p">{field.label}: {field.value}</p>
       )}
     </div>
