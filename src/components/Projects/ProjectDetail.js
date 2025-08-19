0817c5a123fdb9e7434be9f8c47cd4d7325d4e03
MiraLindroos <mira.isabelle27@gmail.com>
2025-08-19 14:28:19 +0300

    added € marks for displaying prices

--- src/components/Projects/ProjectDetail.js
+++ src/components/Projects/ProjectDetail.js
@@ -31,9 +31,9 @@ const ProjectDetail = ({project, totalHours, price, start, end, onChange}) => {
             fields={[
               {label: start && end ? `Aikavälin ${start.toLocaleDateString('fi-Fi', {day: 'numeric', month: 'numeric'})} - ${end.toLocaleDateString('fi-Fi', {day: 'numeric', month: 'numeric'})} yhteenveto` : "Kokonaistuntien yhteenveto"},
               {label: "Tehdyt tunnit", value: start && end ? `${totalHours} h` : `${project.hours.toFixed(1)} h`},
-              {label: "Alviton hinta (Alv 0)", value: project.hourRate ? project.hourRate * (start && end ? totalHours : project.hours) : project.fixedRate ? project.fixedRate : "Ei hintaa" },
-              {label: "Alvin määrä (Alv 25,5%)", value: project.hourRate ? ((project.hourRate * (start && end ? totalHours : project.hours)) * 0.255).toFixed(2) : project.fixedRate ? ((project.fixedRate) * 0.255).toFixed(2) : "Ei hintaa" },
-              {label: "Kokonaishinta", value: project.hourRate ? ((project.hourRate * (start && end ? totalHours : project.hours)) * 1.255).toFixed(2) : project.fixedRate ? ((project.fixedRate) * 1.255).toFixed(2) : "Ei hintaa" },
+              {label: "Alviton hinta (Alv 0)", value: project.hourRate ? `${project.hourRate * (start && end ? totalHours : project.hours)} €` : project.fixedRate ? `${project.fixedRate} €` : "Ei hintaa" },
+              {label: "Alvin määrä (Alv 25,5%)", value: project.hourRate ? `${((project.hourRate * (start && end ? totalHours : project.hours)) * 0.255).toFixed(2)} €` : project.fixedRate ? `${((project.fixedRate) * 0.255).toFixed(2)} €` : "Ei hintaa" },
+              {label: "Kokonaishinta", value: project.hourRate ? `${((project.hourRate * (start && end ? totalHours : project.hours)) * 1.255).toFixed(2)} €` : project.fixedRate ? `${((project.fixedRate) * 1.255).toFixed(2)} €` : "Ei hintaa" },
               {label: "Kaikki projektille tehdyt tunnit", value: `${project.hours.toFixed(1)} h`},
             ]}
           />
