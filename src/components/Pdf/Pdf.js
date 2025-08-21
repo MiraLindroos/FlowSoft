60de50d3f120018508a372611012951abddcb2c2
MiraLindroos <mira.isabelle27@gmail.com>
2025-08-21 10:29:57 +0300

    added styling to pdf

--- src/components/Pdf/Pdf.js
+++ src/components/Pdf/Pdf.js
@@ -1,6 +1,9 @@
 import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
+  page: {
+    margin: 10,
+  },
   title: {
     margin: 10,
     padding: 10,
@@ -10,22 +13,29 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    borderTop: '1px solid black'
+    borderTop: '1px solid black',
   },
   text: {
-    padding: 5,
+    padding: 8,
+    width: '25%',
+    fontSize: '0.8rem',
+    textAlign: 'center',
+    borderLeft: '1px solid black',
   }
 })
 
 const Pdf = ({project, projectsEntries, start, end}) => (
   <Document>
-    <Page size="A4">
+    <Page size="A4" style={styles.page}>
       <View style={styles.title}>
         <Text>Työtunnit projektille: {project.name}</Text>
         <Text>Aikaväliltä : {start} - {end}</Text>
       </View>
-      <View>
-        <Text>Päivä | Tunnit | Kilometrit | Muistiinpanot</Text>
+      <View style={styles.section}>
+        <Text style={styles.text}>Päivä</Text>
+        <Text style={styles.text}>Tunnit</Text>
+        <Text style={styles.text}>Kilometrit</Text>
+        <Text style={styles.text}>Muistiinpanot</Text>
       </View>
       {projectsEntries.map((entry) => {
           return (
