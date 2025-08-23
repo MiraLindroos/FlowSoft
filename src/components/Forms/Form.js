af800f5a18256abdea8bf47a16511a29c36a5dd0
MiraLindroos <mira.isabelle27@gmail.com>
2025-08-23 11:15:21 +0300

    added comments

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -2,25 +2,36 @@ import "./Forms.css"
 import { useFormContext } from "react-hook-form"
 
 const Form = ({fields}) => {
-  const { register } = useFormContext()
+  // useFormContext allows access to the form methods provided by FormProvider
+  // Register allows to register inputs and apply validation rules to React Hook Form
+  // The register function returns props like name, onChange etc. which is why we need to spread it like this {...register()}
+  const { register } = useFormContext() // Retrieve all hook methods
 
   return (
     <form className="form-area" >
+      {/* Go through given fields and display them */}
       {fields.map((field, index) => 
         <div className="form-item" key={index}>
+          {/* Display the label and if the field is required show * next to the label */}
           <label>{field.label} {field.required && <span>*</span>}</label>
+          {/* If field type is select */}
           {field.type==="select" ? (
+            // Dropdown for selecting a project
             <select {...register(`${field.name}`, { required: field.required })}>
               <option value="">Valitse projekti</option>
+              {/* Display fields options */}
               {field.options.map((option, index) => 
                 <option key={index} value={JSON.stringify(option)}>
                   {option.name}
                 </option>
               )}
             </select>
+            // If field type is textarea
           ) : field.type==="textarea" ? (
+            // Multiline text field
             <textarea {...register(`${field.name}`, { required: field.required })} />
           ) : (
+            // If field type is other than select or textarea
             <input 
               {...register(`${field.name}`, { required: field.required })}
               type={field.type}
