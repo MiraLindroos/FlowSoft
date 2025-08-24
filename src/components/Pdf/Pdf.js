3c50071df9cb5bd67bc47451b0a2f3a86d225947
MiraLindroos <mira.isabelle27@gmail.com>
2025-08-24 15:33:20 +0300

    pdf styling

--- src/components/Pdf/Pdf.js
+++ src/components/Pdf/Pdf.js
@@ -3,24 +3,29 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 const styles = StyleSheet.create({
   page: {
     margin: 10,
+    display: 'flex',
+    alignItems: 'center'
   },
   title: {
     margin: 10,
     padding: 10,
     textAlign: 'center'
   },
+  container: {
+    border: '1px solid black',
+    width: '95%'
+  },
   section: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderTop: '1px solid black',
+    justifyContent: 'space-between'
   },
   text: {
     padding: 8,
     width: '25%',
     fontSize: '0.8rem',
     textAlign: 'center',
-    borderLeft: '1px solid black',
+    borderTop: '1px solid black'
   }
 })
 
@@ -31,25 +36,24 @@ const Pdf = ({project, projectsEntries, start, end}) => (
         <Text>Työtunnit projektille: {project.name}</Text>
         <Text>Aikaväliltä : {start} - {end}</Text>
       </View>
-      <View style={styles.section}>
-        <Text style={styles.text}>Päivä</Text>
-        <Text style={styles.text}>Tunnit</Text>
-        <Text style={styles.text}>Kilometrit</Text>
-        <Text style={styles.text}>Muistiinpanot</Text>
-      </View>
-      {projectsEntries.map((entry) => {
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.text}>Päivä</Text>
+          <Text style={styles.text}>Tunnit</Text>
+          <Text style={styles.text}>Kilometrit</Text>
+          <Text style={styles.text}>Muistiinpanot</Text>
+        </View>
+        {projectsEntries.map((entry) => {
           return (
             <View key={entry.id} style={styles.section}>
               <Text style={styles.text}>{entry.startTime.toDate().toLocaleDateString()}</Text>
               <Text style={styles.text}>{entry.hours} h</Text>
-              <Text style={styles.text}>{entry.kilometers} km</Text>
+              <Text style={styles.text}>{entry.kilometers ? entry.kilometers : 0} km</Text>
               <Text style={styles.text}>{entry.memo}</Text>
             </View>
           )
-        }
-      )
-      }
-
+        })}
+      </View>
     </Page>
   </Document>
 )
