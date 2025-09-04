e52a229f52a8be04714e4c0f2a5012402c19b4d5
MiraLindroos <mira.isabelle27@gmail.com>
2025-09-04 13:08:26 +0300

    fixed wrong indentation

--- src/components/Pdf/Pdf.js
+++ src/components/Pdf/Pdf.js
@@ -17,16 +17,16 @@ const Pdf = ({project, projectsEntries = [], start, end, totalHours, totalTravel
           <Text style={[styles.text, styles.flex1]}>Kilometrit</Text>
           <Text style={[styles.text, styles.flex2]}>Muistiinpanot</Text>
         </View>
-          {projectsEntries.map((entry) => {
-            return (
-              <View key={entry.id} style={styles.section}>
-                <Text style={[styles.text, styles.flex1]}>{entry.startTime.toDate().toLocaleDateString()}</Text>
-                <Text style={[styles.text, styles.flex1]}>{entry.hours} h</Text>
-                <Text style={[styles.text, styles.flex1]}>{entry.kilometers ? entry.kilometers : 0} km</Text>
-                <Text style={[styles.text, styles.flex2]}>{entry.memo}</Text>
-              </View>
-            )
-          })}
+        {projectsEntries.map((entry) => {
+          return (
+            <View key={entry.id} style={styles.section}>
+              <Text style={[styles.text, styles.flex1]}>{entry.startTime.toDate().toLocaleDateString()}</Text>
+              <Text style={[styles.text, styles.flex1]}>{entry.hours} h</Text>
+              <Text style={[styles.text, styles.flex1]}>{entry.kilometers ? entry.kilometers : 0} km</Text>
+              <Text style={[styles.text, styles.flex2]}>{entry.memo}</Text>
+            </View>
+          )
+        })}
         <View style={[styles.infoRow, { borderTop: '1px solid #ccc'}]}>
           <Text style={[styles.text, styles.flex1]}>Yht.</Text>
           <Text style={[styles.text, styles.flex1]}>{totalHours} h</Text>
